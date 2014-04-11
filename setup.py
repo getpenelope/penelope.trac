771194dacb132e907a3e8fdd97917256c055e7c0
Andrew Mleczko <andrew@mleczko.net>
2014-04-11 14:58:19 +0200

    Back to development: 1.2.8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 
 
 setup(name='penelope.trac',
-      version='1.2.7',
+      version='1.2.8.dev0',
       description='Penelope: Trac integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
